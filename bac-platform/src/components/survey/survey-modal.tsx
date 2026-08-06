bf1c52e40a5447be2fb7b53de1e5b00d34401035
moudabouacha09-lab <moudabouacha09@gmail.com>
2026-08-06 13:07:47 +0100

    feat: Rename Technical Math branch to Engineering in survey modal

--- bac-platform/src/components/survey/survey-modal.tsx
+++ bac-platform/src/components/survey/survey-modal.tsx
@@ -6,7 +6,7 @@ import React, { useEffect, useState } from "react";
 const BRANCHES = [
   "علوم تجريبية",
   "رياضيات",
-  "تقني رياضي",
+  "هندسات",
   "تسيير وإقتصاد",
   "آداب وفلسفة",
   "لغات أجنبية"
